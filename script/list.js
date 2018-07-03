5348c166e0be65f8a6b0ada4e6d930f7e99e8667
Théo <the.perrin@outlook.fr>
2018-07-03 18:00:29 +0200

    tab list + controller swipe

--- script/list.js
+++ script/list.js
@@ -1,8 +1,9 @@
 var txt = "";
-function renameList(){
+
+function renameList() {
     var name = prompt("Enter a name for your list:", txt);
-    if(name == null || name === ""){
-        if(txt === "")
+    if (name == null || name === "") {
+        if (txt === "")
             txt = "MyList";
     } else {
         txt = name;
@@ -11,6 +12,39 @@ function renameList(){
     title.html(txt);
 }
 
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function handleTouchStart(evt) {
+    xDown = evt.touches[0].clientX;
+    yDown = evt.touches[0].clientY;
+}
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+        if (xDiff > 0) {
+            console.log("left");
+        } else {
+            console.log("right");
+        }
+    }
+}
+
+/* reset values */
+xDown = null;
 
 
 var deleted = null;
@@ -18,13 +52,13 @@ var app = angular.module('todoApp', []);
 var btnSave = angular.element(document.querySelector('.save-list'));
 var btnCancelRemove = angular.element(document.querySelector('.cancel-remove'));
 
-app.controller('todoList', function($scope) {
-    $scope.todos = [ {text:'Learn AngularJS', done:true},
-        {text:'build an AngularJS app', done:false}];
+app.controller('todoList', function ($scope) {
+    $scope.todos = [{text: 'Learn AngularJS', done: true},
+        {text: 'build an AngularJS app', done: false}];
 
-    $scope.addTodo = function(item) {
-        if($scope.todoText !== '') {
-            $scope.todos.push({text:$scope.todoText, done:false});
+    $scope.addTodo = function (item) {
+        if ($scope.todoText !== '') {
+            $scope.todos.push({text: $scope.todoText, done: false});
             $scope.todoText = '';
         } else {
             $scope.todos.push(item)
@@ -32,9 +66,10 @@ app.controller('todoList', function($scope) {
     };
 
     $scope.cancelRemove = function () {
-      $scope.addTodo(deleted);
+        console.log(deleted);
+        $scope.addTodo(deleted);
     };
-    
+
     $scope.removeTodo = function (idx) {
         deleted = $scope.todos[idx];
         $scope.todos.splice(idx, 1);
